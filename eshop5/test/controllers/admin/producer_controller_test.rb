0a3ff99cf64afa7e73c79875ce1e790fc90840da
JoseCorrero <jose_correro@hotmail.com>
2019-03-19 19:00:05 +0100

    Producer Controller Test

--- eshop5/test/controllers/admin/producer_controller_test.rb
+++ eshop5/test/controllers/admin/producer_controller_test.rb
@@ -1,39 +1,68 @@
 require 'test_helper'
 
 class Admin::ProducerControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get admin_producer_new_url
+  fixtures :producers
+
+  test "new" do
+    get '/admin/producer/new'
     assert_response :success
   end
 
-  test "should get create" do
-    get admin_producer_create_url
-    assert_response :success
+  test "create" do
+    num_producers = Producer.count
+    post '/admin/producer/create', :params => { :producer => { :name => 'Marvel Comics' }}
+    assert_response :redirect
+    assert_redirected_to :action => 'index'
+    assert_equal num_producers + 1, Producer.count
   end
 
-  test "should get edit" do
-    get admin_producer_edit_url
-    assert_response :success
+  test "edit" do
+    get '/admin/producer/edit', :params => { :id => 1 }
+    assert_select 'input' do
+      assert_select '[type=?]', 'text'
+      assert_select '[name=?]', 'producer[name]'
+      assert_select '[value=?]', 'Apress'
+    end
   end
 
-  test "should get update" do
-    get admin_producer_update_url
-    assert_response :success
+  test "update" do
+    post '/admin/producer/update', :params => { :id => 1, :producer => { :name => 'Apress.com' }}
+    assert_response :redirect
+    assert_redirected_to :action => 'show', :id => 1
+    assert_equal 'Apress.com', Producer.find(1).name
   end
 
-  test "should get destroy" do
-    get admin_producer_destroy_url
-    assert_response :success
+  test "destroy" do
+    assert_difference(Producer, :count, -1) do
+      post '/admin/producer/destroy', :params => { :id => 1 }
+      assert_equal flash[:notice], 'Productora Apress eliminada correctamente.'
+      assert_response :redirect
+      assert_redirected_to :action => 'index'
+      get '/admin/producer/index'
+      assert_response :success
+      assert_select 'div#notice', 'Productora Apress eliminada correctamente.'
+    end
   end
 
-  test "should get show" do
-    get admin_producer_show_url
+  test "show" do
+    get '/admin/producer/show', :params => { :id => 1 }
     assert_response :success
+    assert_template 'admin/producer/show'
+    assert_not_nil assigns(:producer)
+    assert assigns(:producer).valid?
+    assert_select 'div#content' do
+      assert_select 'h1', Producer.find(1).name
+    end
   end
 
-  test "should get index" do
-    get admin_producer_index_url
+  test "index" do
+    get '/admin/producer/index'
     assert_response :success
+    assert_select 'table' do
+      assert_select 'tr', Producer.count + 1
+    end
+    Producer.find_each do |a|
+      assert_select 'td', a.name
+    end
   end
-
 end
