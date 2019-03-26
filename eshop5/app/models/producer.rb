29525b205631f07b156c12d310002293ef660ca5
SantiGodoy <godoy.programmer@gmail.com>
2019-03-26 08:59:42 +0100

    Añadidas validaciones en español

--- eshop5/app/models/producer.rb
+++ eshop5/app/models/producer.rb
@@ -1,5 +1,6 @@
+# coding: utf-8
 class Producer < ApplicationRecord
-  validates_presence_of :name
-  validates_uniqueness_of :name
-  validates_length_of :name, :in => 2..255
+  validates_presence_of :name, :message => 'Debe introducir un nombre para la productora'
+  validates_uniqueness_of :name, :message => 'El nombre de la productora ya existe'
+  validates_length_of :name, :in => 2..255, :message => 'El nombre de la productora no se ajusta al tamaño límite (2...255)'
 end
