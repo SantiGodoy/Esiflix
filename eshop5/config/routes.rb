2cc7ab57352a9ab2e2811e6fc1da65148d2122f9
SantiGodoy <godoy.programmer@gmail.com>
2019-03-26 12:21:02 +0100

    CRUD de la entidad Director

--- eshop5/config/routes.rb
+++ eshop5/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root :to => 'about#index'
 
   get 'about' => 'about#index'
+  get 'admin/director' => 'admin/director#index'
   get 'admin/producer' => 'admin/producer#index'
 
   get 'about/index'
@@ -14,7 +15,16 @@ Rails.application.routes.draw do
   post 'admin/producer/destroy'
   get 'admin/producer/show'
   get 'admin/producer/show/:id' => 'admin/producer#show'
-  get 'admin/producer/index'  
+  get 'admin/producer/index'
+
+  get 'admin/director/new'
+  post 'admin/director/create'
+  get 'admin/director/edit'
+  post 'admin/director/update'
+  post 'admin/director/destroy'
+  get 'admin/director/show'
+  get 'admin/director/show/:id' => 'admin/director#show'
+  get 'admin/director/index'  
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
