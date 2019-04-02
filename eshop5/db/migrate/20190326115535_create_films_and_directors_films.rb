803617824e434dc6b05c4e6143f313def08179c5
SantiGodoy <godoy.programmer@gmail.com>
2019-04-02 10:57:07 +0200

    Creado fichero de migración Directors_Films

--- eshop5/db/migrate/20190326115535_create_films_and_directors_films.rb
+++ eshop5/db/migrate/20190326115535_create_films_and_directors_films.rb
@@ -1,6 +1,37 @@
 class CreateFilmsAndDirectorsFilms < ActiveRecord::Migration[5.1]
-  def change
-    create_table :films_and_directors_films do |t|
+  def up
+    create_table :films do |t|
+       t.string :title, :limit => 255, :null => false
+       t.integer :producer_id, :limit => 8, :null => false
+       t.datetime :produced_at
+       t.string :kind, :limit => 15
+       t.text :blurb
+       t.integer :duration
+       t.float :price
+       t.timestamps
+    end
+    
+    create_table :directors_films do |t|
+      t.integer :director_id, :limit => 8, :null => false
+      t.integer :film_id, :limit => 8, :null => false
+      t.timestamps
+    end
+
+    say_with_time 'Adding foreing keys' do
+      # Add foreign key reference to directors_films table
+      execute 'ALTER TABLE directors_films ADD CONSTRAINT fk_directors_films_directors
+              FOREIGN KEY (director_id) REFERENCES directors(id) ON DELETE CASCADE'      
+      execute 'ALTER TABLE directors_films ADD CONSTRAINT fk_directors_films_films
+              FOREIGN KEY (film_id) REFERENCES films(id) ON DELETE CASCADE'
+      # Add foreign key reference to films table
+      execute 'ALTER TABLE films ADD CONSTRAINT fk_films_producers
+              FOREIGN KEY (producer_id) REFERENCES producers(id) ON DELETE CASCADE'
     end
   end
+  
+  def self.down
+    drop_table :films
+    drop_table :directors_films
+  end
+  
 end
--- eshop5/db/schema.rb
+++ eshop5/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190326094409) do
+ActiveRecord::Schema.define(version: 20190326115535) do
 
   create_table "directors", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string "first_name", null: false
@@ -19,10 +19,35 @@ ActiveRecord::Schema.define(version: 20190326094409) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "directors_films", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.bigint "director_id", null: false
+    t.bigint "film_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["director_id"], name: "fk_directors_films_directors"
+    t.index ["film_id"], name: "fk_directors_films_films"
+  end
+
+  create_table "films", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.string "title", null: false
+    t.bigint "producer_id", null: false
+    t.datetime "produced_at"
+    t.string "kind", limit: 15
+    t.text "blurb"
+    t.integer "duration"
+    t.float "price", limit: 24
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["producer_id"], name: "fk_films_producers"
+  end
+
   create_table "producers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "directors_films", "directors", name: "fk_directors_films_directors", on_delete: :cascade
+  add_foreign_key "directors_films", "films", name: "fk_directors_films_films", on_delete: :cascade
+  add_foreign_key "films", "producers", name: "fk_films_producers", on_delete: :cascade
 end
