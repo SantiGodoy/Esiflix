9596baa333ff036dbd5541873325aa70cebb5e85
SantiGodoy <godoy.programmer@gmail.com>
2019-04-23 09:12:03 +0200

    Añadidos mensajes en español para las validaciones de Películas

--- eshop5/app/models/film.rb
+++ eshop5/app/models/film.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 class Film < ApplicationRecord
   has_and_belongs_to_many :directors
   belongs_to :producer
@@ -9,13 +10,13 @@ class Film < ApplicationRecord
   validates_attachment :cover_image,
   :content_type => { :content_type => ["image/jpeg", "image/gif", "image/png"] }
 
-  validates_length_of :title, :in => 1..255
-  validates_presence_of :producer
-  validates_presence_of :directors
-  validates_presence_of :produced_at
-  validates_numericality_of :duration, :only_integer => true
-  validates_numericality_of :price
-  validates_length_of :kind, :in => 1..15
+  validates_length_of :title, :in => 1..255, :message => 'El título de la película no está dentro de los límites [1...255].'
+  validates_presence_of :producer, :message => 'Debe seleccionar una productora.'
+  validates_presence_of :directors, :message => 'Debe seleccionar uno o más directores.'
+  validates_presence_of :produced_at, :message => 'Debe seleccionar una fecha de producción.'
+  validates_numericality_of :duration, :only_integer => true, :message => 'La duración debe ser un número.'
+  validates_numericality_of :price, :message => 'El precio debe ser un número.'
+  validates_length_of :kind, :in => 1..15, :message => 'El formato de la película no se ajusta al tamaño [1...15].'
 
   def director_names
     self.directors.map{|director| director.name}.join(", ")
