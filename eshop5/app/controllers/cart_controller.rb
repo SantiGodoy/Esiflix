0952932b6eba55e7ab36087159670a5225e1c963
SantiGodoy <godoy.programmer@gmail.com>
2019-05-14 16:32:41 +0200

    Añadido Ajax y su test

--- eshop5/app/controllers/cart_controller.rb
+++ eshop5/app/controllers/cart_controller.rb
@@ -5,35 +5,51 @@ class CartController < ApplicationController
   def add
     @film = Film.find params[:id]
     @page_title = 'Añadir artículo'
-    if request.post?
-      @item = @cart.add params[:id]
-      flash[:cart_notice] = "Añadido <em>#{@item.film.title}</em>."
-      redirect_to :controller => 'catalog'
-    else
-      render :controller => 'cart', :action => 'add', :template => 'cart/add'
+    respond_to do |format|
+      format.js { @item = @cart.add params[:id]
+        flash.now[:cart_notice] = "Añadido <em>#{@item.film.title}</em>"
+        render :controller => 'cart', :action => 'add_with_ajax' }
+      format.html { if request.post?
+        @item = @cart.add params[:id]
+        flash[:cart_notice] = "Añadido <em>#{@item.film.title}</em>."
+        redirect_to :controller => 'catalog'
+      else
+        render :controller => 'cart', :action => 'add', :template => 'cart/add'
+      end }
     end
   end
-
+  
   def remove
     @film = Film.find params[:id]
     @page_title = 'Eliminar artículo'
-    if request.post?
-      @item = @cart.remove params[:id]
-      flash[:cart_notice] = "Eliminado <em>#{@item.film.title}</em>."
-      redirect_to :controller => 'catalog'
-    else
-      render :controller => 'cart', :action => 'remove', :template => 'cart/remove'
+    respond_to do |format|
+      format.js { @item = @cart.remove params[:id]
+        flash.now[:cart_notice] = "Eliminado <em>#{@item.film.title}</em>"
+        render :controller => 'cart', :action => 'remove_with_ajax' }
+      format.html { if request.post?
+        @item = @cart.remove params[:id]
+        flash[:cart_notice] = "Eliminado <em>#{@item.film.title}</em>."
+        redirect_to :controller => 'catalog'
+      else
+        render :controller => 'cart', :action => 'remove', :template => 'cart/remove'
+      end }
     end
   end
 
   def clear
     @page_title = 'Vaciar carrito'
-    if request.post?
-      @cart.cart_items.destroy_all
-      flash[:cart_notice] = "Carrito vaciado."
-      redirect_to :controller => 'catalog'
-    else
-      render :controller => 'cart', :action => 'clear', :template => 'cart/clear'
+    respond_to do |format|
+      format.js { @cart.cart_items.destroy_all
+        flash.now[:cart_notice] = "Carrito vaciado"
+        render :controller => 'cart', :action => 'clear_with_ajax' }
+      format.html { if request.post?
+        @cart.cart_items.destroy_all
+        flash[:cart_notice] = "Carrito vaciado."
+        redirect_to :controller => 'catalog'
+      else
+        render :controller => 'cart', :action => 'clear', :template => 'cart/clear'
+      end }
     end
   end
 end
+  
--- eshop5/test/controllers/cart_controller_test.rb
+++ eshop5/test/controllers/cart_controller_test.rb
@@ -12,12 +12,35 @@ class CartControllerTest < ActionDispatch::IntegrationTest
     assert_equal 1, Cart.find(@request.session[:cart_id]).cart_items.size
   end
 
+  test "add_xhr" do
+    assert_difference(CartItem, :count) do
+      post '/cart/add', :params => { :id => 4 }, :xhr => true
+    end
+    assert_response :success
+    assert_select_jquery :html, '#shopping_cart' do
+      assert_select 'li#cart_item_4', /Pro Rails E-Commerce 4th Edition/
+    end
+    assert_equal 1, Cart.find(@request.session[:cart_id]).cart_items.size
+  end
+  
   test "remove" do
     post '/cart/add', :params => { :id => 4 }
     assert_equal [Film.find(4)], Cart.find(@request.session[:cart_id]).films
 
     post '/cart/remove', :params => { :id => 4 }
     assert_equal [], Cart.find(@request.session[:cart_id]).films
+  end
+
+  test "remove_xhr" do
+    assert_difference(CartItem, :count) do
+      post '/cart/add', :params => { :id => 4 }, :xhr => true
+    end
+    
+    assert_response :success
+    post '/cart/remove', :params => { :id => 4 }, :xhr => true
+    assert_select_jquery :html, '#shopping_cart' do
+      assert_select 'li#cart_item_4', false
+    end
   end
 
   test "clear" do
@@ -28,5 +51,17 @@ class CartControllerTest < ActionDispatch::IntegrationTest
     assert_response :redirect
     assert_redirected_to :controller => 'catalog'
     assert_equal [], Cart.find(@request.session[:cart_id]).films
+  end
+
+  test "clear_xhr" do
+    assert_difference(CartItem, :count) do
+      post '/cart/add', :params => { :id => 4 }, :xhr => true
+    end
+    
+    assert_response :success
+    post '/cart/clear', :xhr => true
+    assert_select_jquery :html, '#shopping_cart' do
+      assert_select 'li#cart_item_4', false
+    end
   end
 end
